9e0af91e27f0ba650847534762ee406318be24b8
Veelsgaard93 <veelsgaard@gmail.com>
2016-05-04 10:07:11 +0200

    Viewmodel med INotifyPropertyChanged

---
+++ Monument/Monument/ViewModels/StatueViewmodel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Monument.Annotations;
+
+namespace Monument.ViewModels
+{
+    class StatueViewmodel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ICommand AddStatueCommand { get; set; }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        #region MyRegion
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        } 
+        #endregion
+    }
+}
