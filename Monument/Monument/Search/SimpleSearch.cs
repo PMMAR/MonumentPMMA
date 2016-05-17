e4c8cd342cb3c9156fe9ab84e3767bec3e893307
Allando <alla2634@edu.easj.dk>
2016-05-18 00:57:12 +0200

    SimpleSearch class added

---
+++ Monument/Monument/Search/SimpleSearch.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monument.Search
+{
+    class SimpleSearch
+    {
+        //string connString = ConfigurationManager.ConnectionStrings["datasource = ohm-server.database.windows.net; initial catalog = MonumentDB; persist security info=True;user id = Ohmsen93; password=Mads1234;MultipleActiveResultSets=True;App=EntityFramework"].ConnectionString;
+
+
+        //SqlConnection con = new SqlConnection(connString);
+
+
+        //    SqlConnection conn = new SqlConnection();
+        //    conn.ConnectionString = @"Data Source=ohm-server.database.windows.net/MonumentDB;Integrated Security=True";
+        //    conn.Open();
+        //    SqlDataAdapter adapter = new SqlDataAdapter("select * from dbo.statuer where Navn like '%" + {0} + "%'", conn);
+        //    DataTable dt = new DataTable();
+        //adapter.Fill(dt);
+
+
+        //    ListView.DataSource = dt;
+        //    ListView.Visible = true;
+        //    conn.Close();
+    }
+}
